3e58b40bd90e7e77e44d91eaedcfad544d8e8564
Rostyk <rostykrom@yahoo.com>
2021-11-08 07:07:09 +0200

    ngx translate is introduced

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GoogleMapsModule } from '@angular/google-maps';
-import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  HttpClientJsonpModule,
+  HttpClient,
+} from '@angular/common/http';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +26,14 @@ import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
     GoogleMapsModule,
     HttpClientModule,
     HttpClientJsonpModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'ua',
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
