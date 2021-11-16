d6e5c6d2bf605234917d899d3f965f902f33dfb3
Rostyk <rostykrom@yahoo.com>
2021-11-16 07:01:33 +0200

    firebase is added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,17 @@ import {
 } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideDatabase,getDatabase } from '@angular/fire/database';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+import { provideMessaging,getMessaging } from '@angular/fire/messaging';
+import { providePerformance,getPerformance } from '@angular/fire/performance';
+import { provideRemoteConfig,getRemoteConfig } from '@angular/fire/remote-config';
+import { provideStorage,getStorage } from '@angular/fire/storage';
 
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -34,8 +45,20 @@ export function HttpLoaderFactory(http: HttpClient) {
       },
       defaultLanguage: 'ua',
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideDatabase(() => getDatabase()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions()),
+    provideMessaging(() => getMessaging()),
+    providePerformance(() => getPerformance()),
+    provideRemoteConfig(() => getRemoteConfig()),
+    provideStorage(() => getStorage()),
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'tryfb-4eb43',
+    appId: '1:120604648368:web:a0cf2d11aae6620d0b2c75',
+    databaseURL: 'https://tryfb-4eb43.firebaseio.com',
+    storageBucket: 'tryfb-4eb43.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAZYxDihg5oNfEsp_NDH8uwo9ajn2zmllc',
+    authDomain: 'tryfb-4eb43.firebaseapp.com',
+    messagingSenderId: '120604648368',
+  },
   production: true
 };
